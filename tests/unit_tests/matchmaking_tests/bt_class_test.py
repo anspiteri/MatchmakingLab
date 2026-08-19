b4c66d5c45e8f0c0c62ab789a137dbb1311b9eb9
An Spiteri <an@anspiteri.com>
2026-08-19 18:45:53 +1000

    Added skeleton for the remaining Bradley Terry unit tests

---
+++ tests/unit_tests/matchmaking_tests/bt_class_test.py
@@ -0,0 +1,24 @@
+"""
+bt_class_test.py
+~~~~~~~~~~~~~~~~~~~~
+
+This module tests the external functions from the BradleyTerry matchmaking
+strategy. These functions are concerned with providing an interface for the
+strategy within the platform execution environment.
+"""
+
+
+def test_class_init():
+    pass
+
+
+def test_setup_player_features():
+    pass
+
+
+def test_update_player_features():
+    pass
+
+
+def test_run_algorithm():
+    pass
---
+++ tests/unit_tests/matchmaking_tests/bt_global_optimisation_test.py
@@ -0,0 +1,16 @@
+"""
+bt_global_optimisation_test.py
+~~~~~~~~~~~~~~~~~~~~
+
+This module tests a group of functions from the BradleyTerry matchmaking
+strategy. These functions are concerned with optimising the best configuration
+of matches for a give set of queued players.
+"""
+
+
+def queue_matching_function_test():
+    pass
+
+
+def test_greedy_optimisation():
+    pass
